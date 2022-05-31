914f3ef363f624fa20402f4debf386bd6c908f1c
Leonardo Muniz <leonardomunizoficial@gmail.com>
2022-05-31 20:37:15 -0300

    usando promise.all

--- aula1/promise_2.js
+++ aula1/promise_2.js
@@ -1,14 +1,23 @@
-function getRandomNumber(min, max) {
+function getRandomNumber(min, max, time) {
     if (min > max) [max,min] = [min,max]
-    
     return new Promise(resolve => {
-        const fator = max - min + 1
-        const aleatorio = parseInt(Math.random() * fator) + min
-        resolve(aleatorio)
+        setTimeout( () =>{
+            const fator = max - min + 1
+            const aleatorio = parseInt(Math.random() * fator) + min
+            resolve(aleatorio)
+        }, time)
     })
 }
 
-getRandomNumber(10,11)
-.then(num => num * 10)
-.then(numX10 => `O numero gerado foi ${numX10}`)
-.then(console.log)
+function gerarNumeros(){
+    // promise.all recebe um array com varias promisses e so quando todas forem finalizadas
+    // o promise.all retorna todos os resultados em um array 
+    return Promise.all([
+        getRandomNumber(1, 60, 1000),
+        getRandomNumber(312, 42, 500),
+        getRandomNumber(1, 12, 5000),
+        getRandomNumber(3, 23, 20),
+    ])
+}
+
+gerarNumeros().then(console.log)
